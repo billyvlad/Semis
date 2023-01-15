f1a62025b0fac7b7cf38ec867bdd1695b0e6c08b
vladya <vladyslav.davydov100@gmail.com>
2023-01-16 02:44:51 +0300

    dz4

---
+++ DZ4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int current = -1 * number;
+while(current <= number)
+{
+    Console.Write($"{current}, ");
+    current++;
+}
--- semis4/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.Write("Введите число ");
-int number = Convert.ToInt32(Console.ReadLine());
-int current = -1 * number;
-while(current <= number)
-{
-    Console.Write($"{current}, ");
-    current++;
-}
