29bc5782716dec238fd57ed7f4501602f32b9bcf
vladya <vladyslav.davydov100@gmail.com>
2023-01-19 01:39:22 +0300

    DZ2 to seminar 2

---
+++ seminars2/DZ2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numbers = Convert.ToString(number);
+if (numbers.Length > 2)
+{
+  Console.WriteLine("третья цифра - " + numbers[2]);
+}
+else 
+{
+  Console.WriteLine("третьей цифры нет");
+}
+
+
+
