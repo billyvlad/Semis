ba43d31a3eb1ca57b2d1a577522e6406771d5f3c
vladya <vladyslav.davydov100@gmail.com>
2023-01-19 00:17:06 +0300

    Решение 3 задачи семинара

--- seminars2/semi2.3/Program.cs
+++ seminars2/semi2.3/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA % numberB == 0)
+{
+    Console.WriteLine(numberA + ", " + numberB + " - второе число Кратно первому, остаток " + numberA % numberB);
+}
+    else
+    { 
+        Console.WriteLine(numberA + ", " + numberB + " -второе число не Кратно первому, остаток " + numberA % numberB );
+
+    }
+
+
+
+    
+
