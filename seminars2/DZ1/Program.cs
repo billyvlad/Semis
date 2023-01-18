8066ebae49266287a3e4e6038e60cb90ee3b0d29
vladya <vladyslav.davydov100@gmail.com>
2023-01-19 02:19:43 +0300

    добавил еще одно решение к 1 задаче

--- seminars2/DZ1/Program.cs
+++ seminars2/DZ1/Program.cs
@@ -3,3 +3,22 @@ int number = Convert.ToInt32(Console.ReadLine());
 int dig2 = number % 100 / 10;
 int result = dig2;
 Console.WriteLine($"{number} -> {result}");
+
+
+
+
+
+
+Console.WriteLine("Введите число: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int dig3 = number % 100 / 10;
+int result1 = dig3;
+if(numbers > 1000)
+{
+    Console.WriteLine("Ты задал больше чем трехзначное число");
+}
+else
+{
+    Console.WriteLine($"{number} -> {result}");
+
+}
