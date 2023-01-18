2c48512fbba806eb0771199718f77d5a71f4ebd7
vladya <vladyslav.davydov100@gmail.com>
2023-01-19 00:55:36 +0300

    DZ1 to seminar 2

---
+++ seminars2/DZ1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int dig2 = number % 100 / 10;
+int result = dig2;
+Console.WriteLine($"{number} -> {result}");
---
+++ seminars2/semi2.4/Program.cs
@@ -0,0 +1,11 @@
+﻿int num1 = 5;
+int num2 = 7;
+
+if(num1 == 5 || num2 == 8) // будет True, но если поставить && то будет False
+{
+    Console.WriteLine("all good");
+}
+else
+{
+    Console.WriteLine("wrong");
+}
