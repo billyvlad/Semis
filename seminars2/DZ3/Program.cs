c5f9b23a58ad29c93a60d9fb2d5382b9be2d9a6b
vladya <vladyslav.davydov100@gmail.com>
2023-01-19 01:52:37 +0300

    DZ3 to seminar 2

---
+++ seminars2/DZ3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите цифру дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number == 6 || number == 7) // будет True, но если поставить && то будет False
+{
+    Console.WriteLine("Этот день - выходной");
+}
+else if (number < 1 || number > 7) 
+{
+    Console.WriteLine("если у тебя столько дней в неделе, то пора на выходной)))))");
+}
+else
+{
+    Console.WriteLine("Это рабочий день, иди страдай)");
+}
